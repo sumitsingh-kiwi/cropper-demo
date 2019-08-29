8d84887833e46c998a2894b5bddbd6404498134c
sumitsingh-kiwi <sumit.singh@kiwitech.com>
2019-08-29 11:21:00 +0530

    removed .pyc files

--- cropper_demo/settings.py
+++ cropper_demo/settings.py
@@ -130,5 +130,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
